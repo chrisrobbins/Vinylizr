d87e29fd7ce0c95700effc5069f357325b4a6a70
Chris Robbins <Jaguar85>
2017-07-18 13:13:14 -0500

    created users branch

--- Vinylizr/src/actions/collection-action.js
+++ Vinylizr/src/actions/collection-action.js
@@ -16,7 +16,7 @@ export function fetchCollection() {
 
     //read database when child is added to collection
 
-    fire.database().ref(`${userId}/collection/albums`).on('child_added', snapshot => {
+    fire.database().ref(`users/${userId}/collection/albums`).on('child_added', snapshot => {
       dispatch({
         type: FETCH_COLLECTION,
         payload: snapshot.val().album
@@ -24,7 +24,7 @@ export function fetchCollection() {
       console.log("WTF ", snapshot.val().album);
     });
       //read database when child is removed from collection
-      fire.database().ref(`${userId}/collection/albums`).on('child_removed', snapshot => {
+      fire.database().ref(`users/${userId}/collection/albums`).on('child_removed', snapshot => {
         dispatch({
           type: FETCH_COLLECTION,
           payload: snapshot.val().album
@@ -37,7 +37,7 @@ export function saveCollectionItem(album) {
   let userId = fire.auth().currentUser.uid;
   console.log(userId);
   return dispatch =>
-  fire.database().ref(`${userId}/collection/albums`).push({
+  fire.database().ref(`users/${userId}/collection/albums`).push({
     album:album,
     id: userId
   })
--- Vinylizr/src/actions/wantlist-action.js
+++ Vinylizr/src/actions/wantlist-action.js
@@ -11,14 +11,14 @@ import {
 export function fetchWantlist() {
   let userId = fire.auth().currentUser.uid;
   return dispatch => {
-    fire.database().ref(`${userId}/wantlist/albums`).on('child_added', snapshot => {
+    fire.database().ref(`users/${userId}/wantlist/albums`).on('child_added', snapshot => {
       dispatch({
         type: FETCH_WANTLIST,
         payload: snapshot.val().album
       });
       console.log("WTF ", snapshot.val().album);
     });
-    fire.database().ref(`${userId}/wantlist/albums`).on('child_removed', snapshot => {
+    fire.database().ref(`users/${userId}/wantlist/albums`).on('child_removed', snapshot => {
       dispatch({
         type: FETCH_WANTLIST,
         payload: snapshot.val().album
@@ -31,7 +31,7 @@ export function saveWantlistItem(album) {
   let userId = fire.auth().currentUser.uid;
   console.log(album);
   return dispatch =>
-  fire.database().ref(`${userId}/wantlist/albums`).push({
+  fire.database().ref(`users/${userId}/wantlist/albums`).push({
     album:album,
     id:userId
   })
