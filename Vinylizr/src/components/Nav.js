fc9dbab715542ff35213852147b586a0bbcbe684
chrisrobbins <me@chrisrobbins.io>
2017-07-18 19:44:52 -0500

    organized files

---
+++ Vinylizr/src/components/Nav.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+var {
+  TabBarIOS,
+  Text,
+  View,
+} = ReactNative;
+
+import DeezerSearch from '../screens/DeezerSearch';
+import UserProfile from '../screens/UserProfile';
+import UserCollections from '../screens/UserCollections';
+import WantList from '../screens/WantList';
+
+
+
+class Nav extends React.Component {
+  static title = '<TabBarIOS>';
+  static description = 'Tab-based navigation.';
+  static displayName = 'Vinylizr';
+
+  state = {
+    selectedTab: 'UserCollections',
+  };
+
+
+  render() {
+    return (
+      <TabBarIOS
+        unselectedItemTintColor="yellow"
+        barTintColor="#1A1A1A">
+        <TabBarIOS.Item
+          title=""
+          renderAsOriginal
+          icon={require('../img/collectionIcon.png')}
+          selectedIcon={require('../img/collection_select.png')}
+          selected={this.state.selectedTab === 'UserCollections'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'UserCollections',
+            });
+          }}>
+          <UserCollections />
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title=""
+          renderAsOriginal
+          icon={require('../img/wantlistIcon.png')}
+          selectedIcon={require('../img/wantlistIcon_select.png')}
+          selected={this.state.selectedTab === 'wantList'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'wantList',
+            });
+          }}>
+          <WantList />
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title=""
+          renderAsOriginal
+          icon={require('../img/profile.png')}
+          selectedIcon={require('../img/profile_select.png')}
+          selected={this.state.selectedTab === 'profile'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'profile',
+            });
+          }}>
+          <UserProfile />
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title=""
+          icon={require('../img/search.png')}
+          selectedIcon={require('../img/search_select.png')}
+          renderAsOriginal
+          selected={this.state.selectedTab === 'search'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'search'
+            });
+          }}>
+          <DeezerSearch />
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+}
+
+
+module.exports = Nav;
---
+++ Vinylizr/src/components/app.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { View, StatusBar } from 'react-native';
+import * as firebase from "firebase";
+import { Spinner } from './common';
+import Nav from './Nav';
+import LoginForm from './LoginForm';
+import { connect } from 'react-redux';
+import fire from './fire';
+
+
+
+class App extends Component {
+  state = { loggedIn: null };
+
+componentWillMount() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log("USER: ", user);
+    if (user) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+  });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Nav />;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+
+    <View style={styles.container}>
+      <StatusBar
+       barStyle="light-content"
+      />
+      {this.renderContent()}
+    </View>
+
+    );
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#000000'
+  },
+  status: {
+    backgroundColor: '#1a1a1a'
+  }
+};
+
+
+const mapStateToProps = (state) => {
+    return {
+      ...state
+    }
+}
+// for click events so that dispatches can happen
+// const mapDispatchToProps = (dispatch) => {
+//     return;
+//     }
+//
+
+
+export default connect(mapStateToProps)(App);
